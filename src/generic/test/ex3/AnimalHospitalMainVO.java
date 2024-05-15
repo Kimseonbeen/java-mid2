40eb1b271569051be91eb24914842637302b00e6
Kimseonbeen <ksb9242@gmail.com>
2024-05-15 09:53:09 +0900

    제네릭 완료 !

--- src/generic/test/ex3/AnimalHospitalMainVO.java
+++
@@ -1,9 +0,0 @@
-package generic.test.ex3;
-
-public class AnimalHospitalMainVO {
-
-    public static void main(String[] args) {
-        DogHospital dogHospital = new DogHospital();
-        CatHospital catHospital = new CatHospital();
-    }
-}
